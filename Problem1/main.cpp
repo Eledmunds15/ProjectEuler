b19a9e3129955effbc784480abb45e81489f5662
Ethan Edmunds <eledmunds1@sheffield.ac.uk>
2025-08-09 19:16:27 +0100

    update prob1 sol

--- Problem1/main.cpp
+++ Problem1/main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main() {
     
-    std::cout << "Project Euler - Problem 1:\n";
+    cout << "Project Euler - Problem 1:\n";
 
     int input, sum = 0;
 
-    std::cout << "Input: ";
-    std::cin >> input;
+    cout << "Input: ";
+    cin >> input;
 
     for (int i=1; i<input; i++) {
 
-        // std::cout << i << "\n";
+        // cout << i << "\n";
 
         double check1 = (double)i/3, check2 = (double)i/5;
 
         if (floor(check1)==check1 || floor(check2)==check2) {
-            std::cout << "Found number: " << i << "\n";
+            cout << "Found number: " << i << "\n";
             sum += i;
         } else {}
 
     }
 
-    std::cout << "The answer for " << input << " is: " << sum << "\n";
+    cout << "The answer for " << input << " is: " << sum << "\n";
 
     return 0;
 }
